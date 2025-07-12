7050b446146edfce97392a7b99a315f3436c614f
Danylo <161326427+Daniel2-1@users.noreply.github.com>
2025-07-12 13:51:00 +0200

    finished

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -93,5 +93,3 @@ function updateDate(timeDiff) {
   spanMinutes.textContent = minutes.toString().padStart(2, '0');
   spanSeconds.textContent = seconds.toString().padStart(2, '0');
 }
-
-console.log('s');
